22a669bc5cdf14c031ec5bb57b9371c838dafc6a
Priyadarshan Kharel <159047892+Mercurydarshan@users.noreply.github.com>
2024-09-16 11:13:59 -0400

    Print statements added..........

--- Main.py
+++ Main.py
@@ -3,6 +3,33 @@
 #Print out the result
 
 print("Welcome to Mad Libs!")
+print()
 print("Give me a name: ")
 name=input()
+
+print("Give me your age: ")
+age=input()
+
+print("Give me a adjective: ")
+adj1= input()
+
+print("Give me a verb: ")
+verb1=input()
+
+print("Give me a food name: ")
+food1=input()
+
+print("Give me your favourite animal: ")
+ani1=input()
+
+print("Give me a TV show: ")
+TV=input()
+
+print("Give me a country:  ")
+country=input()
+
+print("Give me a favourite car: ")
+car=input()
+
+
 print("One day",name,"wrote a Mad Lib for class!")
